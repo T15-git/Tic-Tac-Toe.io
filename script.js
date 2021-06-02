7b65fc7d9f901e7486802a46d1a251862b3f0c7d
Tushar <tushar.code05@gmail.com>
2021-06-02 20:05:46 +1100

    wip

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var save = [0,1,2,3,4,5,6,7,8,9]
+var score = [0,0]
 turn = 1;
 function change(i){
     if(save[i]!= i ) return;
@@ -7,7 +8,7 @@ function change(i){
         document.getElementById(i).innerHTML = 'X';
         save[i]='X';
         winner(turn)
-        turn++;
+        turn=2;
         document.getElementById('display').innerHTML = 'Player 2 Turn';
         return;
     }
@@ -15,7 +16,7 @@ function change(i){
     document.getElementById(i).innerHTML = 'O';
     save[i]='O';
     winner(turn)
-    turn--;
+    turn=1
     document.getElementById('display').innerHTML = 'Player 1 Turn';
 }
 
@@ -27,10 +28,29 @@ function winner(win){
 
         document.getElementById('display').innerHTML = '! Player ' + win + ' Is Winner !';
         alert('! Player '+win+' Is Winner !\nPress Ok To Continue');
+        let winn=win;
+        reset(0,winn);
     }
     if(save[1]!=1 && save[2]!=2 && save[3]!=3 && save[4]!=4 && save[5]!=5 && save[6]!=6
         && save[7]!=7 && save[8]!=8 && save[9]!=9){
 
             document.getElementById('display').innerHTML = '! DRAW !';
+            reset(2,2);
         }
+}
+
+function reset(choice,win){
+    for(let i=9;i!=0;i--){
+        document.getElementById(i).innerHTML = '.';
+        save[i]=i;
+    }
+
+   if(choice==0){
+        if(win == 1) return document.getElementById('score').innerHTML = 'Player1<'+ ++score[0] +'> Player2<'+ score[1] +'>';
+        return document.getElementById('score').innerHTML = 'Player1<'+ score[0] +'> Player2<'+ ++score[1] +'>';
+    }
+    if(choice == 1){
+        document.getElementById('score').innerHTML = 'Player1<0> Player2<0>';
+        score = [0,0];
+    }
 }
